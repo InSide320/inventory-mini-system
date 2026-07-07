d340c04a3bfd0a3a0f68819ed43988ad68d16c36
Denys Kud <dekud2109@gmail.com>
2026-07-07 18:36:00 +0300

    feat: enhance CreateProductDto with validation and add global validation pipe

--- backend-nest/src/main.ts
+++ backend-nest/src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { config } from "dotenv";
+import { ValidationPipe } from "@nestjs/common";
 
 config({ path: ".env" });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: false,
+  });
   await app.listen(process.env.NEST_PORT || 4002, "0.0.0.0");
   console.log(`Server started http://localhost:${process.env.NEST_PORT || 4002}`);
 }
--- backend-nest/src/product/dto/create.product.dto.ts
+++ backend-nest/src/product/dto/create.product.dto.ts
@@ -1,8 +1,13 @@
 import { ProductStatus } from "../enum/product.status.enum";
-
+import { IsEnum, IsNumber, IsString, MinLength } from "class-validator";
 export class CreateProductDto {
+  @IsString({ message: "Name must be a string" })
+  @MinLength(3, { message: "Name must be at least 3 characters long" })
   name!: string;
+  @IsNumber()
   quantity!: number;
+  @IsString()
   price!: string;
+  @IsEnum(ProductStatus)
   status?: ProductStatus;
 }
